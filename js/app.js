ae43f017fffed202dae7098ac6271069b9d32432
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-26 13:41:53 +0600

    phones hunter loader

--- js/app.js
+++ js/app.js
@@ -38,12 +38,28 @@ const displayPhones = (phones) => {
     `;
     phonesContainer.appendChild(phoneDiv);
   });
+
+  // stop spinner or loader
+  toggleSpinner(false);
 };
 
+// handel search button click
+
 document.getElementById("btn-search").addEventListener("click", function () {
+  // start loader
+  toggleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   loadPhones(searchText);
 });
+
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById("loader");
+  if (isLoading) {
+    loaderSection.classList.remove("d-none");
+  } else {
+    loaderSection.classList.add("d-none");
+  }
+};
 
 // loadPhones();
